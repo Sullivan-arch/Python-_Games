e668b02c7f9b6fd518b67c259cca3eb78e8c9947
Sullivan A <sullivanabegg8@gmail.com>
2022-10-31 07:55:19 -0400

    Create screen

--- Python_Games.py
+++ Python_Games.py
@@ -1,3 +1,15 @@
 #Sullivan Abegg
 #10/28/22
 #Version 1.0
+
+#Import Libraries Here
+import turtle
+
+#Create screen
+screen = turtle.Screen()
+screen.title("Pong Game")
+screen.bgcolor("White")
+screen.setup(width=1000, height=600)
+
+#Left Paddle
+#
